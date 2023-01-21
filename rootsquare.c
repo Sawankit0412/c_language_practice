9387ce5f03749ef88060af284afc80ee646f3371
ankit <ankit0207.be21@chitkara.edu.in>
2023-01-21 13:16:23 +0530

    square root of no

---
+++ rootsquare.c
@@ -0,0 +1,22 @@
+/* square root of any number to find .
+let's start.
+
+eg = 144 , square root is  12 
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+
+    float a, squar;
+    printf("Enter the no ");
+    scanf("%f", &a);
+    squar = sqrt(a);   // store value of square root (sqrt) in squar.
+    printf("squar root is  %f\n ", squar);
+    return 0;
+}
+
+
+
+
---
+++ square.c
@@ -0,0 +1,15 @@
+/*let's start to find out no of square root.
+
+eg= 5 square in 25;
+ */
+#include<stdio.h>
+int main(){
+
+    int num;
+     //square of no = num;
+    printf("Enter the no ");
+    scanf("%d", &num);
+
+    printf("square of given no %f is",  sqrt(num));
+    return 0;
+}
