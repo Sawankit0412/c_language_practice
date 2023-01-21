b13a6da7dfbdc82e37ad7f2728e9ce0ec45543c5
ankit <ankit0207.be21@chitkara.edu.in>
2023-01-22 00:33:07 +0530

    area of circle

---
+++ areaofcircle.c
@@ -0,0 +1,19 @@
+/* Find area of circle 
+*/
+#include<stdio.h>
+  float pi = 3.14;
+int main()
+{ 
+    float a, r;
+   // pi = 3.14;
+    printf("Enter the value");
+    scanf("%f", &r);
+    a = pi * r * r;
+    printf("area of circle is %f", a);
+    return 0;
+ }
+
+ /* inputput = 5;
+  output = 3.14*5*5=78.5;
+
+ */
