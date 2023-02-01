16ee623ef73ada5edf035e4c204551a29157f654
ankit <ankit0207.be21@chitkara.edu.in>
2023-02-01 16:44:38 +0530

    increment and decrement

---
+++ incredec.c
@@ -0,0 +1,24 @@
+/* increment and decrement function.
+*/
+
+// pre increment is ++i;
+// post increment is i++;
+#include<stdio.h>
+int main(){
+int i = 1;
+// printf("%d \n", i++); //use, then increase.
+// printf("%d \n", i);
+
+// printf("%d \n", ++i); //increase then ,use
+// printf("%d \n", i);
+
+// decrement function
+
+//  printf("%d \n", i--); 
+//  printf("%d \n", i);
+
+printf("%d \n", --i); 
+ printf("%d \n", i);
+
+ return 0;
+}
